f2d0a34a2f91d123cd7657d9ad6e7936b9d57b72
Imtious tanvir <82751228+imtious-tanvir@users.noreply.github.com>
2025-09-03 10:21:57 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,5 +1,28 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 
+#side bar
 st.title("Linear Regression Web Application")
 st.subheader("Data Science")
+
+st.sidebar.header("Upload CSV or Use Sample")
+use_example = st.sidebar.checkbox("Use example dataset")
+
+#load data
+if use_example:
+  df = sns.load_dataset("tips")
+  df = df.dropna()
+  st.success("Loaded sample dataset: 'tips' ")
+else:
+  uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=['csv'])
+  if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+  else:
+    st.warning("Please upload a CSV file or use the example dataset")
+    st.stop()
